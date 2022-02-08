a244112d09f53def4d3d6345a8f8fcba0ee83fbd
bobsabt <bodai.barbara@gmail.com>
2022-02-08 19:16:14 +0100

    get data from api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,27 @@
+import React from 'react';
+import { RadioBrowserApi } from 'radio-browser-api';
 import Navbar from './Components/Navbar';
 import './index.css';
 
 function App() {
+  
+  const api = new RadioBrowserApi('My Radio App');
+
+  // Get data from radio api
+  const setupApi = async () => {
+    const datas = await api.searchStations({limit: 30})
+    .then((data) => {
+      console.log(data)
+    
+    })      
+  };
+
+  React.useEffect(() => {
+      setupApi()
+    }, 
+    []
+  );
+
   return (
     <div className="App">
       <div>
