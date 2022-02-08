b7f7f12f230f398f3b684b8ea79046a9910a4bf2
bobsabt <bodai.barbara@gmail.com>
2022-02-08 21:05:14 +0100

    play component

--- frontend/src/Components/Station.jsx
+++ frontend/src/Components/Station.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 
-const Station = ({ icon, name, setDefaultSrc }) => {
+const Station = ({ icon, name, setDefaultSrc, audiosrc, togglePlay }) => {
   
-   
-
     return (
         <div className='placement station-box'>
             <img src={icon} alt="logo" onError={setDefaultSrc}/>
             <div>
             <p>{name}</p>
-            <button>
+            <button onClick={()=>togglePlay(audiosrc, name, icon)}>
                 <FontAwesomeIcon className='play-btn' icon={faPlayCircle} />
             </button>
             
--- frontend/src/Components/StationsPanel.jsx
+++ frontend/src/Components/StationsPanel.jsx
@@ -1,18 +1,66 @@
 import React from 'react';
 import Station from './Station';
 import Pagination from './Pagination';
+import Play from './Play';
 import radio from "../Logo/radio.png";
 
 
 const StationsPanel = ({ stations, currentPage, setCurrentPage, total, stationsPerPage, name, updateName }) => {
 
+    const [isshowPlay, setIsshowPlay] = React.useState(false);
+    const [isBtnPlay, setIsBtnPlay] = React.useState(false);
+    const [actualRadio, setActualRadio] = React.useState();
+    const [currentSong, setCurrentSong] = React.useState(null);
+
+    const audio = React.useRef(currentSong);
+
     const setDefaultSrc = event => {
         event.target.src = radio;
     };
 
+    const togglePlay = (url, currentRadio, currentIcon) => {
+        const song = url;
+        setActualRadio(currentRadio);
+
+        if (currentSong === song) {
+            audio.current.play();
+        
+        } else {
+            if (audio.current) {
+                audio.current.pause();
+            }
+        }
+        audio.current = new Audio(song);
+        audio.current.play();
+        setIsshowPlay(true)
+        setIsBtnPlay(true)
+    };
+
+    const togglePlayerbtn = () => {
+        if(isBtnPlay){
+            audio.current.pause()
+            setIsBtnPlay(false)  
+        }
+        else{
+            audio.current.play()
+            setIsBtnPlay(true)   
+        }   
+    };
+
+    const handleClose = () => {
+        audio.current.pause()
+        setIsshowPlay(false)
+    };
+
     return (
         <div className='stationpanel'>
-
+            <Play
+                isshowPlay={isshowPlay}
+                isBtnPlay={isBtnPlay}
+                actualRadio={actualRadio}
+                togglePlayerbtn={togglePlayerbtn}
+                handleClose={handleClose}
+            />
             <div className="station-search">
                 <label htmlFor="name">Station name</label>
                 <input id="name" type="text" placeholder='Search station...' value={name} onChange={e => updateName(e.target.value)}/>
@@ -25,7 +73,8 @@ const StationsPanel = ({ stations, currentPage, setCurrentPage, total, stationsP
                         icon={station.favicon !== "" ? station.favicon: radio}
                         name={station.name.length > 20 ? 
                             station.name.substring(0,20) + "..." : station.name} 
-                        setDefaultSrc={setDefaultSrc}     
+                        setDefaultSrc={setDefaultSrc} 
+                        togglePlay={togglePlay}       
                     />
                 )}
             </div>
